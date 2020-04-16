4dc2b7cca520bdb8d952ff2c71206c3a276aae59
FadhilaDwi <fadhiladwikurniawan@gmail.com>
2020-04-16 19:09:23 +0700

    catering

---
+++ Catering/application/controllers/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+class dashboard extends CI_Controller{
+
+    public function index()
+    {
+        $data['hewan'] = $this->model_hewan->tampil_data()->result();
+
+        $this->load->view('template/header');
+        $this->load->view('template/sidebar');
+        $this->load->view('dashboard', $data);
+        $this->load->view('template/footer');
+
+        
+
+        
+
+        
+
+      
+
+    }
+
+}
