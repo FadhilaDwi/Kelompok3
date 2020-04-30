a1eea18eb4c6940a30708298748e4f43369a7882
dodhy <dodhyk29@gmail.com>
2020-04-30 12:55:11 +0700

    tambah api

--- Dodhy/pelanggan/application/controllers/api/Pelanggan.php
+++ Dodhy/pelanggan/application/controllers/api/Pelanggan.php
@@ -61,20 +61,20 @@ class Pelanggan extends REST_Controller{
 
 			
             public function login_post(){
-            $username = $this->input->post('username');
-			$password = $this->input->post('password');
-			//mengubah data string pada database menjadi bentuk array
-			$where = array(
-				'username' => $username,
-				'password' => ($password)
-			);
-            $cek = $this->M_login->cek_login("pelanggan", $where)->num_rows();
-            if(!$cek){
-                $this->response(array('status' => false, 'message' => 'Username dan password salah'), REST_Controller::HTTP_OK);
-            }else{
-                $this->response(array('status' => true, 'message' => 'Silahkan masuk', 'response'=>$cek), REST_Controller::HTTP_BAD_REQUEST); 
-            }
-        }
+				$username = $this->input->post('username');
+				$password = $this->input->post('password');
+				//mengubah data string pada database menjadi bentuk array
+				$where = array(
+					'username' => $username,
+					'password' => ($password)
+				);
+				$cek = $this->M_login->cek_login("pelanggan", $where)->num_rows();
+				if(!$cek){
+					$this->response(array('status' => false, 'message' => 'Username dan password salah'), REST_Controller::HTTP_OK);
+				}else{
+					$this->response(array('status' => true, 'message' => 'Silahkan masuk', 'response'=>$cek), REST_Controller::HTTP_BAD_REQUEST); 
+				}
+        	}
     }
 
 
