debcc72310a533ad72bcf22fd23f5860b981ca77
dodhy <dodhyk29@gmail.com>
2020-05-04 19:21:31 +0700

    baru lagi

---
+++ Utama/pelanggan/application/controllers/loginmitra.php
@@ -0,0 +1,44 @@
+<?php 
+
+	class loginmitra extends Ci_Controller{
+		function __construct(){
+			parent::__construct();
+			$this->load->model('login');
+		}
+
+		public function index()
+
+		{
+			
+			$this->load->view('loginmitra');
+		}
+		
+		public function aksi_login(){
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			//mengubah data string pada database menjadi bentuk array
+			$where = array(
+				'username' => $username,
+				'password' => ($password)
+			);
+			$cek = $this->login->cek_login("mitra", $where)->num_rows(); //melakukan cek tabel user berdasarkan id
+			//jika terdapat id maka melakukan cek data yang diinput dengan database, jika cocok membuka controller dashboard admin.
+			if($cek > 0){
+				$data_session = array(
+					'nama' => $username,
+					'status' => "login"
+				);
+				$this->session->set_userdata($data_session);
+				redirect(base_url('customer/dashboardmitra'));
+			
+			}else{
+				echo "maaf username dan password salah";
+			}
+		}
+		function logout(){
+			$this->session->sess_destroy(); //perintah untuk menghentikan session
+			redirect(base_url('loginmitra')); // pindah ke halaman login
+		}
+
+}
+?>
